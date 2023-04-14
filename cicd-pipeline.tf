ccd2daec0488157cd8c3fc8308177983dc1f3764
raguram99 <ramcloud88@gmail.com>
2023-04-14 15:08:46 +0530

    seound commit

--- cicd-pipeline.tf
+++ cicd-pipeline.tf
@@ -68,7 +68,7 @@ resource "aws_codepipeline" "cicd_pipeline" {
             version = "1"
             output_artifacts = ["tf-code"]
             configuration = {
-                FullRepositoryId = "davoclock/aws-cicd-pipeline"
+                FullRepositoryId = "raguram99/aws-cicd-pipeline"
                 BranchName   = "master"
                 ConnectionArn = var.codestar_connector_credentials
                 OutputArtifactFormat = "CODE_ZIP"
